128220b03d326d5bebf404014044a5e010f9f33f
Brian Wangila <bwanjalah@gmail.com>
2022-08-20 14:27:19 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a+b>c && a+c>b && b+c>a 
+      if a == b && a == c && b == c
+        :equilateral
+      elsif a == b || a == c || b == c
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      begin
+        raise TriangleError
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Not a valid triangle"
+    end
+  end
 end
+
+triangle1 = Triangle.new(5, 7, 6)
+pp triangle1.kind
